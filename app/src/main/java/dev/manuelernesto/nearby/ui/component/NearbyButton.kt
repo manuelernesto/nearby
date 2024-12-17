0412b9831253066dfa13d0ff787d4a96d6c97304
manuelernesto <alemdadox@gmail.com>
2024-12-17 22:29:09 +0100

    setup resources

---
+++ app/src/main/java/dev/manuelernesto/nearby/ui/component/NearbyButton.kt
@@ -0,0 +1,9 @@
+package dev.manuelernesto.nearby.ui.component
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun NearbyButton(modifier: Modifier = Modifier) {
+
+}
