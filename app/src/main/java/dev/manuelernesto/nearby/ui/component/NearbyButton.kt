df4143e18b9c01ec68bc8d6ea06d2b85e8762b85
manuelernesto <alemdadox@gmail.com>
2024-12-19 21:12:15 +0100

    create NearbyButton

--- app/src/main/java/dev/manuelernesto/nearby/ui/component/NearbyButton.kt
+++ app/src/main/java/dev/manuelernesto/nearby/ui/component/NearbyButton.kt
@@ -1,15 +1,24 @@
 package dev.manuelernesto.nearby.ui.component
 
 import androidx.annotation.DrawableRes
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import dev.manuelernesto.nearby.R
+import dev.manuelernesto.nearby.ui.theme.GreenBase
+import dev.manuelernesto.nearby.ui.theme.Typography
 
 @Composable
 fun NearbyButton(
@@ -19,11 +28,23 @@ fun NearbyButton(
     onClick: () -> Unit
 ) {
 
-    Button(modifier = modifier, onClick = onClick) {
-        iconRes?.let {
-            Icon(painter = painterResource(id = iconRes), contentDescription = "Button Icon")
+    Button(
+        modifier = modifier.heightIn(min = 56.dp),
+        shape = RoundedCornerShape(16.dp),
+        colors = ButtonDefaults.buttonColors(
+            containerColor = GreenBase
+        ),
+        onClick = onClick
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            iconRes?.let {
+                Icon(painter = painterResource(id = iconRes), contentDescription = "Button Icon")
+            }
+            text?.let { Text(text = text.uppercase(), style = Typography.labelLarge) }
         }
-        text?.let { Text(text = text) }
     }
 
 }
@@ -33,9 +54,7 @@ fun NearbyButton(
 @Composable
 private fun NearbyButtonPreview() {
     NearbyButton(
-        modifier = Modifier.fillMaxWidth(),
-        text = "Confirm",
-        iconRes = R.drawable.ic_scan
+        modifier = Modifier.fillMaxWidth(), text = "Confirm", iconRes = R.drawable.ic_scan
     ) {
 
     }
