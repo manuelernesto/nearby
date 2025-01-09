c210c83c87946e407f6362e718a9db578a9f13e5
manuelernesto <alemdadox@gmail.com>
2025-01-09 20:13:31 +0100

    create NearbyMarketCard.kt component

---
+++ app/src/main/java/dev/manuelernesto/nearby/data/model/NearbyRule.kt
@@ -0,0 +1,7 @@
+package dev.manuelernesto.nearby.data.model
+
+data class NearbyRule(
+    val id: String,
+    val description: String,
+    val marketId: String
+)
---
+++ app/src/main/java/dev/manuelernesto/nearby/ui/component/market/NearbyMarketCard.kt
@@ -0,0 +1,9 @@
+package dev.manuelernesto.nearby.ui.component.market
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun NearbyMarketCard(modifier: Modifier = Modifier) {
+
+}
