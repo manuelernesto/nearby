4dee77b3786b8bd51fc0f142d39f0524d7ba6fac
manuelernesto <alemdadox@gmail.com>
2025-01-09 20:42:20 +0100

    create NearbyMarketCard.kt component

--- app/src/main/java/dev/manuelernesto/nearby/ui/component/market/NearbyMarketCard.kt
+++ app/src/main/java/dev/manuelernesto/nearby/ui/component/market/NearbyMarketCard.kt
@@ -1,8 +1,40 @@
 package dev.manuelernesto.nearby.ui.component.market
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import dev.manuelernesto.nearby.R
 import dev.manuelernesto.nearby.data.model.Market
+import dev.manuelernesto.nearby.ui.theme.Gray100
+import dev.manuelernesto.nearby.ui.theme.Gray200
+import dev.manuelernesto.nearby.ui.theme.Gray400
+import dev.manuelernesto.nearby.ui.theme.Gray500
+import dev.manuelernesto.nearby.ui.theme.RedBase
+import dev.manuelernesto.nearby.ui.theme.Typography
 
 @Composable
 fun NearbyMarketCard(
@@ -11,4 +43,88 @@ fun NearbyMarketCard(
     onClick: (Market) -> Unit
 ) {
 
+    Card(
+        modifier = modifier
+            .clip(RoundedCornerShape(12.dp))
+            .background(Gray100)
+            .border(width = 1.dp, color = Gray200, shape = RoundedCornerShape(12.dp)),
+        onClick = { onClick(market) }
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(Gray100)
+                .padding(8.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+            Image(
+                modifier = Modifier
+                    .clip(RoundedCornerShape(12.dp))
+                    .fillMaxWidth(0.3f)
+                    .height(IntrinsicSize.Min)
+                    .aspectRatio(ratio = 1f, matchHeightConstraintsFirst = true),
+                contentScale = ContentScale.Crop,
+                painter = painterResource(R.drawable.img_burger),
+                contentDescription = "Store Image"
+            )
+
+            Column {
+                Text(text = market.name, style = Typography.headlineSmall.copy(fontSize = 14.sp))
+                Spacer(modifier = Modifier.height(8.dp))
+                Text(
+                    text = market.description,
+                    maxLines = 2,
+                    overflow = TextOverflow.Ellipsis,
+                    color = Gray500,
+                    style = Typography.bodyLarge.copy(fontSize = 12.sp)
+                )
+                Spacer(modifier = Modifier.height(16.dp))
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.spacedBy(8.dp)
+                ) {
+
+                    Icon(
+                        modifier = Modifier.size(24.dp),
+                        tint = if (market.coupons > 0) RedBase else Gray400,
+                        painter = painterResource(id = R.drawable.ic_ticket),
+                        contentDescription = "Coupons Icon"
+                    )
+
+                    Text(
+                        text = "${market.coupons} Coupons available",
+                        color = Gray400,
+                        style = Typography.bodyMedium.copy(fontSize = 12.sp)
+                    )
+
+                }
+            }
+
+        }
+
+    }
+
+}
+
+@Preview
+@Composable
+private fun NearbyMarketCardPrev() {
+    NearbyMarketCard(
+        market = Market(
+            id = "1",
+            categoryId = "1",
+            name = "Market",
+            description = "Market Description Market DescriptionMarket DescriptionMarket DescriptionMarket DescriptionMarket DescriptionMarket DescriptionMarket Description",
+            address = "Address",
+            coupons = 20,
+            latitude = -1.0,
+            longitude = 5.0,
+            cover = "https://www.google.com",
+            phone = ""
+
+        )
+    ) {
+
+    }
 }
